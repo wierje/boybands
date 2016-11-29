c49b4465c604aeab40a252376986b737cbfd858d
wierje <wier.je@gmail.com>
2016-11-28 18:14:26 -0600

    js for bands

--- boybands.js
+++ boybands.js
@@ -1,78 +1,13 @@
-var bands = [`Boyz II Men`, `NSync`, `New Kids on the Block`, `98 Degrees`, `One Direction`];
-    console.log(`The current value of i is`, i);
-  for (var i = 0; i < bands.length; i += 1);
-    // console.log(`The current value of i is`, i);
+var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
 
 
-
-// function bandBoys() {
-//     for (var i = 0; i < bands.length; i++);
-//     var boys = bands[i];
-//     console.log(boys);
-//     return boys;
-    // document.getElementById('boy-bands').innerHTML;
-// }
-
-//
-// var currentBand = bands.index;
-// console.log(currentBand);
-// var currentBand = bands.index;
-// console.log(currentBand);
-// The number of loops to perform (what if the array changes?)
-// var loopCount = 5;
-
-// Keep track of which band we're on in the loop
-
-
-
-// Get a reference to the appropriate DOM element for bands
-// var bandElement = document.getElementById('boy-bands').innerHTML;
-
-// Get a reference to the appropriate DOM element for vegetables
-// var veggieElement = 'veggies';
-
-// Start looping
-// for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
-
-// Add the band names into the correct <div>
-// currentBand = ???;
-
-
-// Add the veggie names into the correct <div>
-// currentVeggie = ???;
-// }
-
-
-
-
-
-
-/* var veggies = [`Carrots`, `Kale`, `Zucchini`, `Broccoli`, `Squash`];
-
-// The number of loops to perform (what if the array changes?)
-var loopCount = 5;
-
-// Keep track of which band we're on in the loop
-var currentBand = band.index;
-
-// Keep track of which veggie we're on in the loop
-var currentVeggie = veggies.index;
-
-// Get a reference to the appropriate DOM element for bands
-var bandElement = document.getElementById(???);
-
-// Get a reference to the appropriate DOM element for vegetables
-var veggieElement = ???;
-
-// Start looping
-for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
-
-  // Add the band names into the correct <div>
-  currentBand = ???;
-
-
-  // Add the veggie names into the correct <div>
-  currentVeggie = ???;
-
+for (var i = 0; i < bands.length; i += 1) {
+    var currentBand = bands[i];
+    console.log(currentBand);
+    document.getElementById("boy-bands").innerHTML += "<p>" + currentBand + "</p>";
+    console.log("The current value of i is", i);
 }
-*/
+
+
+var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+console.log(vegetables[0]);
