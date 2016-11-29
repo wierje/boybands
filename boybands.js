af194675445f36689f89e830f7ad52da0efdfec2
wierje <wier.je@gmail.com>
2016-11-28 18:42:16 -0600

    js for veggies

--- boybands.js
+++ boybands.js
@@ -1,13 +1,17 @@
 var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
 
+var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
 for (var i = 0; i < bands.length; i += 1) {
     var currentBand = bands[i];
-    console.log(currentBand);
+    console.log("The current boyband is", currentBand);
     document.getElementById("boy-bands").innerHTML += "<p>" + currentBand + "</p>";
-    console.log("The current value of i is", i);
+}
+
+for (var i = 0; i < vegetables.length; i += 1) {
+    var currentVegetable = vegetables[i];
+    console.log("The current veggie is", currentVegetable);
+    document.getElementById("veggies").innerHTML += "<p>" + currentVegetable + "</p>";
 }
 
 
-var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
-console.log(vegetables[0]);
